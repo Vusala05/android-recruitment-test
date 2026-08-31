1569bca182332ad2b13e87659cfbde33a212c10b
Vusala05 <vusaleisgandarova05@gmail.com>
2026-08-31 21:24:36 +0400

    Adding : Activity and Fragment minSdkVersion 21 targetSdkVersion 31

---
+++ app/src/main/java/com/example/socketapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.socketapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.socketapp.databinding.ActivityMainBinding
+import com.example.socketapp.ui.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(binding.fragmentContainer.id, HomeFragment())
+                .commit()
+        }
+    }
+}
